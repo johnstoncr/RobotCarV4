da600d5482737b2d4e4aac38c7de7cd55955068b
johnstoncr <60519878+johnstoncr@users.noreply.github.com>
2023-11-13 10:15:40 -0700

    Add files via upload
    Code to calibrate forward, backward, rotate right and left

---
+++ Robot_Car_calibration.ino
@@ -0,0 +1,128 @@
+#define PWMA 5    // Controls power to right motor
+#define PWMB 6    // Controls power to left motor
+#define AIN 7     // Controls direction of right motor, HIGH = FORWARD, LOW = REVERSE
+#define BIN 8     // Controls direction of right motor, HIGH = FORWARD, LOW = REVERSE
+#define STBY 3    // Place Motors in standby if LOW, Run if HIGH
+
+float distance; 
+int degRot; 
+
+void setup() { 
+  // put your setup code here, to run once: 
+  Serial.begin(9600); 
+pinMode (STBY,OUTPUT);
+pinMode (PWMA,OUTPUT);
+pinMode (PWMB,OUTPUT);
+pinMode (AIN,OUTPUT);
+pinMode (BIN,OUTPUT);
+digitalWrite(STBY,LOW);  // put it in Standby Mode
+} 
+
+void loop() { 
+//    forward(10); 
+//    turnRight(180); 
+//    forward(10); 
+//    turnLeft(180);
+calF(); 
+    while(1==1){ } 
+}
+
+void forward(float distance){ 
+  float time; 
+  digitalWrite(PWMA,HIGH);
+  digitalWrite(PWMB,HIGH);
+  digitalWrite(AIN,HIGH);
+  digitalWrite(BIN,HIGH);
+  time=distance/2.45*1000;  //after calibration the car travelled 12ft 3 inches in 5 seconds
+  
+  digitalWrite(STBY,HIGH); 
+  delay(time);  // move for "time" seconds 
+  stopCar(); 
+}
+
+void backward(float distance){ 
+  float time; 
+//  digitalWrite(STBY,LOW);
+  digitalWrite(PWMA,HIGH);
+  digitalWrite(PWMB,HIGH);
+  digitalWrite(AIN,LOW);
+  digitalWrite(BIN,LOW);
+  time=distance/2.45*1000; 
+  digitalWrite(STBY,HIGH);
+  delay(time); 
+  stopCar(); 
+} 
+
+void turnRight(int deg){ 
+  float time; 
+  //digitalWrite(STBY,LOW);
+  digitalWrite(PWMA,HIGH);
+  digitalWrite(PWMB,HIGH);
+  digitalWrite(AIN,LOW);
+  digitalWrite(BIN,HIGH);        
+  time=deg/345.*1000.; //after calibration the robot rotated 1725 degrees in 5 seconds
+  Serial.println(deg); 
+  digitalWrite(STBY,HIGH);
+  delay(time); 
+  stopCar(); 
+} 
+        
+void turnLeft(float deg){ 
+  float time; 
+//  digitalWrite(STBY,LOW);
+  digitalWrite(PWMA,HIGH);
+  digitalWrite(PWMB,HIGH);
+  digitalWrite(AIN,HIGH);
+  digitalWrite(BIN,LOW);          
+  time=deg/345.*1000.; 
+  Serial.println(deg);
+  digitalWrite(STBY,HIGH); 
+  delay(time); 
+  stopCar(); 
+} 
+
+void stopCar(){ 
+ digitalWrite(STBY, LOW);
+}
+
+//Calibration             } 
+void calF(){ 
+  analogWrite(PWMA,255);
+  analogWrite(PWMB,255);
+  digitalWrite(AIN,HIGH);
+  digitalWrite(BIN,HIGH);
+  digitalWrite(STBY,HIGH);
+  delay(5000); 
+  stopCar(); 
+} 
+
+void calB(){ 
+  digitalWrite(PWMA,HIGH);
+  digitalWrite(PWMB,HIGH);
+  digitalWrite(AIN,LOW);
+  digitalWrite(BIN,LOW);
+  digitalWrite(STBY,HIGH);
+  delay(5000); 
+  stopCar(); 
+} 
+
+void calR(){ 
+  digitalWrite(PWMA,HIGH);
+  digitalWrite(PWMB,HIGH);
+  digitalWrite(AIN,LOW);
+  digitalWrite(BIN,HIGH);
+  digitalWrite(STBY,HIGH);        
+  delay(5000); 
+  stopCar(); 
+} 
+
+void calL(){ 
+  digitalWrite(PWMA,HIGH);
+  digitalWrite(PWMB,HIGH);
+  digitalWrite(AIN,HIGH);
+  digitalWrite(BIN,LOW);
+  digitalWrite(STBY,HIGH);          
+  delay(5000); 
+  stopCar(); 
+}
+              
